837e079bab894631d52e799403f33b419d76b20c
Christopher Vestman <christophervestman@gmail.com>
2022-05-24 14:47:50 +0200

    feat: auth0

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const slTheme = extendTheme({
   colors: {
@@ -26,7 +27,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={slTheme}>
-      <App />
+      <Auth0Provider>
+        <App />
+      </Auth0Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
