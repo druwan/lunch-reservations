e5a94eb87f3034fa53f28823aec04d0ad3204c11
Christopher Vestman <christophervestman@gmail.com>
2022-05-25 13:42:51 +0200

    feat: display as grid

--- client/src/Views/Home.jsx
+++ client/src/Views/Home.jsx
@@ -1,22 +1,18 @@
-import { Flex, Grid, Heading } from '@chakra-ui/react';
+import { Container, Flex } from '@chakra-ui/react';
 import React from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
+import ContentGrid from '../component/ContentGrid/ContentGrid';
 
 const Home = () => {
-  const { isAuthenticated, user } = useAuth0();
-
-  if (isAuthenticated) {
-    return (
-      <Flex height={'100vh'} alignItems='center' justifyContent='center'>
-        <Heading>Welcome {user.given_name} </Heading>
-      </Flex>
-    );
-  }
-
   return (
-    <Flex height={'100vh'} alignItems='center' justifyContent={'center'}>
-      <Grid>Menu</Grid>
-    </Flex>
+    <Container maxW={'container.xl'} centerContent>
+      <Flex
+        height={'100vh'}
+        alignItems='center'
+        justifyContent={'space-around'}
+      >
+        <ContentGrid />
+      </Flex>
+    </Container>
   );
 };
 
