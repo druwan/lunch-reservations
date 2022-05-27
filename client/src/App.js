38df554a4e6f0e21d63bf48e92c0d33bcba667e4
Christopher Vestman <christophervestman@gmail.com>
2022-05-27 10:41:15 +0200

    refactor: switched disp. order

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Container, Flex } from '@chakra-ui/react';
+import Calendar from './component/Calendar/Calendar';
 import Navbar from './component/Navbar/Navbar';
 import Home from './Views/Home';
 
 const App = () => {
   return (
     <Flex direction={'column'} minHeight={'100vh'}>
-      {/* <Navbar /> */}
-      <Home />
+      <Navbar />
+      <Box>
+        <Calendar />
+        <Home />
+      </Box>
     </Flex>
   );
 };
