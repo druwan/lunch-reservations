3a7a5c399c03422efd2122e62df518a520f419b9
Christopher Vestman <christophervestman@gmail.com>
2022-05-27 18:49:05 +0200

    feat: add more controllers + error handling

--- server/controllers/customerController.js
+++ server/controllers/customerController.js
@@ -60,11 +60,55 @@ export const signUp = async (req, res) => {
   }
 };
 
-export const getCustomers = async (req, res) => {
+export const createCustomer = async (req, res) => {
   try {
-    const customerConfig = await customer.find();
+    // check if customer email already exist
+    const ifExist = await Customer.findOne({ email: req.body.email }).exec();
+    if (ifExist) {
+      return res.status(409).json({ message: 'email already exist' });
+    }
+
+    const newCustomer = await Customer.create({
+      name: req.body.name,
+      email: req.body.email,
+    });
+
+    newCustomer.save();
+    res
+      .status(200)
+      .send(`Successfully created new customer ${newCustomer.name}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const getAllCustomers = async (req, res) => {
+  try {
+    const customerConfig = await Customer.find();
     res.status(200).json(customerConfig);
   } catch (error) {
     res.status(500).json({ message: error });
+  }
+};
+
+export const getOneCustomer = async (req, res) => {
+  const customer_id = req.params.id;
+  try {
+    const customer = await Customer.findById(customer_id);
+    res.status(200).json(customer);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export const deleteCustomer = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const customerToDelete = await Customer.findByIdAndDelete(id);
+    res
+      .status(200)
+      .send(`Successfully deleted customer ${customerToDelete.name}`);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 };
