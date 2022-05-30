983c33aa9eada0acdb8e33dc99dd6a42e5706420
Christopher Vestman <christophervestman@gmail.com>
2022-05-30 11:51:48 +0200

    fix: add references to orders and dish

--- server/models/customerModel.js
+++ server/models/customerModel.js
@@ -4,7 +4,10 @@ const customerSchema = new mongoose.Schema({
   customer_id: String,
   name: { type: String, required: true },
   email: { type: String, required: true },
-  orders: [mongoose.SchemaTypes.String],
+  orders: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Order',
+  },
 });
 
 export default mongoose.model('Customer', customerSchema);
--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose';
 const orderSchema = new mongoose.Schema({
   order_id: String,
   orderDate: { type: Date, default: new Date() },
-  dishes: [mongoose.SchemaTypes.String],
+  dishes: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'Dish',
+  },
 });
 
 const Order = mongoose.model('Order', orderSchema);
