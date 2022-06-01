7633fdbbad0643bfcc361807b21719a4a4523ce1
Christopher Vestman <christophervestman@gmail.com>
2022-06-01 08:13:31 +0200

    feat: customer email set to unique

--- server/models/customerModel.js
+++ server/models/customerModel.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const customerSchema = new mongoose.Schema({
   customer_id: String,
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   orders: {
     type: [mongoose.SchemaTypes.ObjectId],
     ref: 'Order',
