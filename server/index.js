0ce9fc94b1f2b850476dc854954494bbbd800900
Christopher Vestman <christophervestman@gmail.com>
2022-06-02 11:56:28 +0200

    chore: del all auth-rel

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import 'dotenv/config';
-import { getToken, checkJwt, checkScopes } from './middlewares/auth.js';
 
 import customerRoutes from './routes/customersRoutes.js';
 import dishesRoutes from './routes/dishesRoutes.js';
@@ -16,25 +15,6 @@ app.use(express.json());
 app.use('/api/customers', customerRoutes);
 app.use('/api/dishes', dishesRoutes);
 app.use('/api/orders', orderRoutes);
-
-// This route doesn't need authentication
-app.get('/api/public', (req, res) => {
-  res.json({
-    message: `Hello from a public endpoint! You don't need to be authenticated to see this.`,
-  });
-});
-
-// This route needs authentication
-app.get('/api/protected', checkJwt, (req, res) => {
-  try {
-    res.status(200).json({
-      message:
-        'Hello from a private endpoint! You need to be authenticated to see this.',
-    });
-  } catch (error) {
-    res.status(401).json({ message: error.message });
-  }
-});
 
 const dbConnect = async () => {
   try {
