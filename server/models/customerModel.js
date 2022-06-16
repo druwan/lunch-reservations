538b14492fbf9d21a0ca141511166d6cf684555e
Christopher Vestman <christophervestman@gmail.com>
2022-06-16 10:31:23 +0200

    fix: updated customerModel

--- server/models/customerModel.js
+++ server/models/customerModel.js
@@ -4,10 +4,12 @@ const customerSchema = new mongoose.Schema({
   customer_id: String,
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  orders: {
-    type: [mongoose.SchemaTypes.ObjectId],
-    ref: 'Order',
-  },
+  orders: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: 'Order',
+    },
+  ],
 });
 
 export default mongoose.model('Customer', customerSchema);
