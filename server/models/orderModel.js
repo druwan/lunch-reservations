68cedc754bdcebfca50c05a861207527b6cfe2f6
Christopher Vestman <christophervestman@gmail.com>
2022-06-16 15:25:47 +0200

    fix: updated orderModel

--- server/models/orderModel.js
+++ server/models/orderModel.js
@@ -3,10 +3,12 @@ import mongoose from 'mongoose';
 const orderSchema = new mongoose.Schema({
   order_id: String,
   orderDate: { type: Date, default: new Date() },
-  dishes: {
-    type: [mongoose.SchemaTypes.ObjectId],
-    ref: 'Dish',
-  },
+  dishes: [
+    {
+      type: [mongoose.SchemaTypes.ObjectId],
+      ref: 'Dish',
+    },
+  ],
   fulfilled: {
     type: Boolean,
     required: true,
